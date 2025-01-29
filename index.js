900701474dbea6afd4388ac256a0279486cf2d37
henrique1837 <bongador@protonmail.com>
2025-01-29 11:01:27 -0600

    Reacting from webapp data using ctx.telegram.answerWebAppQuery. Need to figure out how to define the id

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ console.log('Environment variables loaded.');
 
 const { Telegraf } = require('telegraf');
 
+const Datastore = require('nedb');
+const path = require('path');
+
 if (!process.env.BOT_TOKEN) {
   console.error('Error: BOT_TOKEN is not defined in the .env file');
   process.exit(1); // Terminate process if BOT_TOKEN is not defined
@@ -11,6 +14,11 @@ if (!process.env.BOT_TOKEN) {
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 console.log('Telegraf instance created.');
+
+// Initialize the NeDB database
+const dbPath = path.join('./data', 'bot.db');
+const db = new Datastore({ filename: dbPath, autoload: true });
+
 
 bot.use((ctx, next) => {
   console.log('Received update:', ctx.update);
@@ -26,7 +34,7 @@ bot.start((ctx) => {
           {
             text: 'Open miniapp',
             web_app: {
-              url: 'https://3775-189-216-171-48.ngrok-free.app'
+              url: 'https://db71-177-238-22-24.ngrok-free.app'
             }
           }
         ]
@@ -40,6 +48,7 @@ bot.start((ctx) => {
 bot.on('message', (ctx) => {
   console.log('Message received.');
   const message = ctx.message;
+  console.log(ctx)
   if (message.web_app_data) {
     const rawData = message.web_app_data.data;
     console.log('Raw data received from WebApp:', rawData);
@@ -52,13 +61,40 @@ bot.on('message', (ctx) => {
       return ctx.reply('Error parsing received data.');
     }
 
-    if (data.action === 'connection') {
-      ctx.reply('Received "connection" action from miniapp. All OK!');
+    if (data.action === 'get_pending_swaps'){
+      const query = {
+        status: "pending"
+      }
+      db.find(query).limit(10).exec();
+    } else if (data.action === 'connection') {
+      console.log(ctx.webAppData)
+      ctx.telegram.answerWebAppQuery(ctx.message.chat.id,{
+        id: ctx.message.chat.id,
+        type: "article",
+        input_message_content: {
+          message_text: "All ok, connected" // Your JSON data as a string
+        },
+        title: 'test'
+      });
     } else if (data.action === 'create_swap') {
       const { recipient, amount, hashLock } = data;
-      ctx.reply(`Swap created. Recipient: ${recipient}, Amount: ${amount}, HashLock: ${hashLock}`);
-    } else {
-      ctx.reply(`Action received: ${data.action}. I don't have logic for this!`);
+      ctx.telegram.answerWebAppQuery(ctx.message.chat.id,{
+        id: ctx.message.chat.id,
+        type: "article",
+        input_message_content: {
+          message_text: "All ok, connected" // Your JSON data as a string
+        },
+        title: 'test'
+      });
+    } else if(ctx.webAppData.query_id){
+      ctx.telegram.answerWebAppQuery(ctx.message.chat.id,{
+        id: ctx.message.chat.id,
+        type: "article",
+        input_message_content: {
+          message_text: "All ok, connected" // Your JSON data as a string
+        },
+        title: 'test'
+      });
     }
   } else {
     ctx.reply('Hello, you can type /start to see the WebApp button.');
