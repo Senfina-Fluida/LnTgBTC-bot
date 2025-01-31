7c4950a6666633bc486b01d04a5b43ef40d1a011
henrique1837 <bongador@protonmail.com>
2025-01-30 19:57:21 -0600

    Allowing start performing swap, opening the miniapp to do it. Added commands

--- index.js
+++ index.js
@@ -56,165 +56,7 @@ async function listSwaps(ctx, query, title) {
   }
 }
 
-
-bot.use((ctx, next) => {
-  console.log('Received update:', ctx.update);
-  return next();
-});
-
-bot.start(async (ctx) => {
-  console.log('/start command received.');
-  const query = { status: "pending" };
-  const chatId = ctx.chat.id;
-  const docs = await queryDocuments(query);
-  const webAppUrl = `${MINIAPP_URL}/?params=${encodeURIComponent(JSON.stringify(docs))}}`;
-  console.log(webAppUrl)
-  ctx.reply('Hello! Press the button to open the miniapp:', {
-    reply_markup: {
-      keyboard: [
-        [
-          {
-            text: 'Open miniapp',
-            web_app: {
-              url: webAppUrl
-            }
-          }
-        ]
-      ],
-      resize_keyboard: true,
-      one_time_keyboard: true
-    }
-  });
-});
-bot.command('listSwaps', async (ctx) => {
-  const query = {};
-  await listSwaps(ctx, query, "Here are all the swaps:");
-});
-
-bot.command('myPendingSwaps', async (ctx) => {
-  const query = { status: "pending", chatId: ctx.message.chat.id };
-  await listSwaps(ctx, query, "Here are your pending swaps:");
-});
-
-bot.command('mySelectedSwaps', async (ctx) => {
-  const query = { status: "selected", chatId: ctx.message.chat.id };
-  await listSwaps(ctx, query, "Here are your selected swaps:");
-});
-
-bot.command('selectSwap', async (ctx) => {
-  const swapId = ctx.message.text.split(' ')[1]; // Extract swapId from command
-
-  if (!swapId) {
-    return ctx.reply("Please provide a swap ID.  Example: /selectSwap 12345");
-  }
-
-  const query = {
-    _id: swapId,
-  };
-
-  try {
-    const docs = await queryDocuments(query);
-
-    if (docs.length === 0) {
-      return ctx.reply("No swap with that ID found.");
-    }
-
-    const swap = docs[0];
-
-    // Inline Keyboard for Confirmation
-    await ctx.reply(`Are you sure you want to select swap ${swapId}?`, {
-      reply_markup: {
-        inline_keyboard: [
-          [{ text: 'Yes', callback_data: `confirm_swap_${swapId}` }],
-          [{ text: 'No', callback_data: 'cancel_swap' }],
-        ],
-      },
-    });
-  } catch (error) {
-    console.error("Error finding swap:", error);
-    return ctx.reply("An error occurred. Please try again later.");
-  }
-});
-
-// Command to create a swap
-bot.command('createSwap', async (ctx) => {
-  const params = ctx.message.text.split(' ').slice(1).join(' '); // Get parameters after command
-  const [ destination, amount] = params.split(' ');  // Split parameters
-
-  if (!destination || !amount || isNaN(amount) || parseFloat(amount) <= 0) {
-    return ctx.reply("Please provide destination, and a valid amount. Example: /createSwap Lightning 10");
-  }
-
-  const doc = {
-    status: "pending",
-    source: destination !== "TON" ? "TON" : "Lightning",
-    destination: destination,
-    amount: parseFloat(amount),
-    chatId: ctx.chat.id,
-  };
-
-  try {
-    const newSwap = await insertDocument(doc);
-    const query = {chatId: ctx.message.chat.id, status: "pending"}
-    const docs = await queryDocuments(query);
-
-    ctx.reply(`Swap created with ID: ${newSwap._id}. Open miniapp to view all pending.`, {
-      reply_markup: {
-        keyboard: [
-          [{
-          text: "Open Web App",
-          web_app: { url: `${MINIAPP_URL}/myPendingSwaps?params=${encodeURIComponent(JSON.stringify(docs))}` }
-          }]
-        ],
-        resize_keyboard: true,
-        one_time_keyboard: true
-      }
-    });
-  } catch (error) {
-    console.error("Error creating swap:", error);
-    ctx.reply("An error occurred while creating the swap.");
-  }
-});
-
-bot.command('selectSwap', async (ctx) => {
-  const swapId = ctx.message.text.split(' ')[1]; // Extract swapId from command
-
-  if (!swapId) {
-    return ctx.reply("Please provide a swap ID.  Example: /selectSwap 12345");
-  }
-
-  const query = {
-    _id: swapId,
-    status: "pending"
-  };
-
-  try {
-    const docs = await queryDocuments(query);
-
-    if (docs.length === 0) {
-      return ctx.reply("No swap with that ID found.");
-    }
-
-    const swap = docs[0];
-
-    // Inline Keyboard for Confirmation
-    await ctx.reply(`Are you sure you want to select swap ${swapId}?`, {
-      reply_markup: {
-        inline_keyboard: [
-          [{ text: 'Yes', callback_data: `confirm_swap_${swapId}` }],
-          [{ text: 'No', callback_data: 'cancel_swap' }],
-        ],
-      },
-    });
-  } catch (error) {
-    console.error("Error finding swap:", error);
-    return ctx.reply("An error occurred. Please try again later.");
-  }
-});
-
-
-bot.action(/confirm_swap_(.+)/, async (ctx) => {
-  const swapId = ctx.match[1];
+async function startSwap(ctx,swapId){
   try {
       let query = {
         _id: swapId,
@@ -261,7 +103,7 @@ bot.action(/confirm_swap_(.+)/, async (ctx) => {
           reply_markup: {
             inline_keyboard: [
               [{
-                text: "Open Web App",
+                text: "Start Swap",
                 web_app: { url: `${MINIAPP_URL}/startSwap?params=${encodeURIComponent(JSON.stringify(swap))}` }
               }]
             ]
@@ -273,12 +115,213 @@ bot.action(/confirm_swap_(.+)/, async (ctx) => {
     console.error("Error updating swap:", error);
     return ctx.reply("An error occurred during update.");
   }
+};
+
+
+bot.use((ctx, next) => {
+  console.log('Received update:', ctx.update);
+  return next();
+});
+
+bot.start(async (ctx) => {
+  console.log('/start command received.');
+  const query = { status: "pending" };
+  const chatId = ctx.chat.id;
+  const docs = await queryDocuments(query);
+  const webAppUrl = `${MINIAPP_URL}/?params=${encodeURIComponent(JSON.stringify(docs))}}`;
+  console.log(webAppUrl)
+  ctx.reply('Hello! Press the button to open the miniapp:', {
+    reply_markup: {
+      keyboard: [
+        [
+          {
+            text: 'Open miniapp',
+            web_app: {
+              url: webAppUrl
+            }
+          }
+        ]
+      ],
+      resize_keyboard: true,
+      one_time_keyboard: true
+    }
+  });
+});
+bot.command('list', async (ctx) => {
+  const query = {status: "pending"};
+  await listSwaps(ctx, query, "Here are all the swaps:");
+});
+
+bot.command('mypending', async (ctx) => {
+  const query = { status: "pending", chatId: ctx.message.chat.id };
+  await listSwaps(ctx, query, "Here are your pending swaps:");
+});
+
+bot.command('myselected', async (ctx) => {
+  const query = { status: "selected", chatId: ctx.message.chat.id };
+  await listSwaps(ctx, query, "Here are your selected swaps:");
+});
+
+// Command to create a swap
+bot.command('create', async (ctx) => {
+  const params = ctx.message.text.split(' ').slice(1).join(' '); // Get parameters after command
+  const [ destination, amount] = params.split(' ');  // Split parameters
+
+  if (!destination || !amount || isNaN(amount) || parseFloat(amount) <= 0) {
+    return ctx.reply("Please provide destination, and a valid amount. Example: /create Lightning 10");
+  }
+
+  const doc = {
+    status: "pending",
+    source: destination !== "TON" ? "TON" : "Lightning",
+    destination: destination,
+    amount: parseFloat(amount),
+    chatId: ctx.chat.id,
+  };
+
+  try {
+    const newSwap = await insertDocument(doc);
+    const query = {chatId: ctx.message.chat.id, status: "pending"}
+    const docs = await queryDocuments(query);
+
+    ctx.reply(`Swap created with ID: ${newSwap._id}. Open miniapp to view all pending.`, {
+      reply_markup: {
+        keyboard: [
+          [{
+          text: "Open Web App",
+          web_app: { url: `${MINIAPP_URL}/myPendingSwaps?params=${encodeURIComponent(JSON.stringify(docs))}` }
+          }]
+        ],
+        resize_keyboard: true,
+        one_time_keyboard: true
+      }
+    });
+  } catch (error) {
+    console.error("Error creating swap:", error);
+    ctx.reply("An error occurred while creating the swap.");
+  }
+});
+
+bot.command('select', async (ctx) => {
+  const swapId = ctx.message.text.split(' ')[1]; // Extract swapId from command
+
+  if (!swapId) {
+    return ctx.reply("Please provide a swap ID.  Example: /select 12345");
+  }
+
+  const query = {
+    _id: swapId,
+    status: "pending"
+  };
+
+  try {
+    const docs = await queryDocuments(query);
+
+    if (docs.length === 0) {
+      return ctx.reply("No swap with that ID found.");
+    }
+
+    const swap = docs[0];
+
+    // Inline Keyboard for Confirmation
+    await ctx.reply(`Are you sure you want to select swap ${swapId}?`, {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: 'Yes', callback_data: `confirm_swap_${swapId}` }],
+          [{ text: 'No', callback_data: 'cancel_swap' }],
+        ],
+      },
+    });
+  } catch (error) {
+    console.error("Error finding swap:", error);
+    return ctx.reply("An error occurred. Please try again later.");
+  }
+});
+
+
+bot.command('finish', async (ctx) => {
+  const swapId = ctx.message.text.split(' ')[1]; // Extract swapId from command
+
+  if (!swapId) {
+    return ctx.reply("Please provide a swap ID.  Example: /finish 12345");
+  }
+
+  const query = {
+    _id: swapId,
+    status: "selected",
+    selectorChatId: ctx.chat.id,
+  };
+
+  try {
+    const docs = await queryDocuments(query);
+
+    if (docs.length === 0) {
+      return ctx.reply("No swap with that ID that you have selected before.");
+    }
+
+    const swap = docs[0];
+
+    // Inline Keyboard for Confirmation
+    if(swap.destination === "Lightning"){
+      await bot.telegram.sendMessage(
+        swap.chatId,
+        `Your swap ${swapId}, has been selected. Time to lock tgBTC`,{
+          reply_markup: {
+            inline_keyboard: [
+              [{
+                text: "Open Web App",
+                web_app: { url: `${MINIAPP_URL}/startSwap?params=${encodeURIComponent(JSON.stringify({...swap,fromTON: false}))}` }
+              }]
+            ]
+          }
+        }
+      );
+      ctx.reply("Swap selected. You will receive a lightning invoice to pay using the miniapp and finish the swap");
+
+    } else {
+      await bot.telegram.sendMessage(
+        swap.chatId,
+        `Your swap ${swapId}, has been selected. You will receive a lightning invoice to pay using the miniapp and finish the swap`
+      );
+      console.log(`${MINIAPP_URL}/startSwap?params=${encodeURIComponent(JSON.stringify({...swap,fromTON:true}))}`)
+      ctx.reply("Swap selected, time to lock tgBTC",{
+        reply_markup: {
+          inline_keyboard: [
+            [{
+              text: "Start Swap",
+              web_app: { url: `${MINIAPP_URL}/startSwap?params=${encodeURIComponent(JSON.stringify({...swap,fromTON:true}))}` }
+            }]
+          ]
+        }
+      });
+    }
+  } catch (error) {
+    console.error("Error finding swap:", error);
+    return ctx.reply("An error occurred. Please try again later.");
+  }
+});
+
+bot.action(/confirm_swap_(.+)/, async (ctx) => {
+  const swapId = ctx.match[1];
+  await startSwap(ctx,swapId);
 });
 
 bot.action('cancel_swap', (ctx) => {
   ctx.reply("Swap selection cancelled.");
 });
-
+bot.command('help', (ctx) => {
+  ctx.reply(
+    `Available Commands:\n` +
+    `- /start: Open the miniapp.\n` +
+    `- /list: List all swaps.\n` +
+    `- /mypending: List your pending swaps.\n` +
+    `- /myselected: List your selected swaps.\n` +
+    `- /create [destination] [amount]: Create a new swap.\n` +
+    `- /select [swapId]: Select a specific swap. \n`,
+    `- /finish [swapId]: Finalize a specific swap.\n`,
+    { parse_mode: 'Markdown' } // Use Markdown for formatting
+  );
+});
 bot.on('message', async (ctx) => {
   console.log('Message received.');
   const message = ctx.message;
@@ -327,43 +370,32 @@ bot.on('message', async (ctx) => {
       });
       
     } else if (data.action === 'select_swap') {
-      const query = {
-        _id: data.swapId,
-      }
-      let docs = await queryDocuments(query);
-      console.log(docs);
-      if(docs.length === 0){
-        return ctx.reply("No swap with that id");
-      }
-      const swap = docs[0];
-      console.log(ctx.message.chat)
-      await updateDocument(
-        { _id: swap._id},
-        { $set : {status: "selected" , selectorChatId: ctx.message.chat.id}}
-      );
-      ctx.reply("Swap selected", {
-        reply_markup: {
-          keyboard: [
-            [{
-            text: "Open Web App",
-            web_app: { url: `${MINIAPP_URL}/?params=${encodeURIComponent(JSON.stringify({ userId: 123, name: "John Doe" }))}` }
-            }]
-          ],
-          resize_keyboard: true,
-          one_time_keyboard: true
-        }
-      });
-
+      await startSwap(ctx,data.swapId);
     } 
 
   } else {
-    ctx.reply('Hello, you can type /start to see the WebApp button.');
+    ctx.reply('Hello, you can type /start to see the WebApp button or /help to see all commands.');
   }
 });
 
 
 
-
+bot.telegram.setMyCommands([
+  { command: 'start', description: 'Open the miniapp' },
+  { command: 'list', description: 'List all swaps' },
+  { command: 'mypending', description: 'List your pending swaps' },
+  { command: 'myselected', description: 'List your selected swaps' },
+  { command: 'create', description: 'Create a new swap (e.g., /create Lightning 10)' },
+  { command: 'select', description: 'Select a specific swap (e.g., /select 12345)' },
+  { command: 'finish', description: 'Finalize a specific swap (e.g., /finish 12345)' },
+  { command: 'help', description: 'Show available commands' },
+])
+  .then(() => {
+    console.log('Bot commands set successfully.');
+  })
+  .catch((err) => {
+    console.error('Error setting bot commands:', err);
+  });
 
 
 
